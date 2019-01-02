aba0dea340f45c74f0fb4f74f29c798435ee8e15
Luis Ruiz <roco65@gmail.com>
2019-01-03 00:10:24 +0100

    timeline for activity, pending to calculate range and improve ui

--- js/activity/ActivityPresenter.js
+++ js/activity/ActivityPresenter.js
@@ -3,6 +3,7 @@
     function ActivityPresenter(Context)
     {
         this.interactor = Context.getActivityInteractor();
+        this.networkInteractor = Context.getNetworkInteractor();
        
         this.activityView = Context.getActivityView(this);
         this.activityView.init();
@@ -10,6 +11,49 @@
 
     Object.defineProperties(ActivityPresenter.prototype,
     {
+        getCameras : {
+            value: function()
+            {
+                var self = this;
+                
+                var cameras = [];
+                
+                this.networkInteractor.getNetworks(new blink.listeners.BaseDecisionListener(
+                    function(data)
+                    {
+                        var numNetworks = data.networks.length;
+                        var i = 0;
+                        
+                        $.each( data.networks, function( key, value )
+                        {
+                            self.networkInteractor.getNetwork(value.id, new blink.listeners.BaseDecisionListener(
+                            function(data)
+                            {
+                                i++;
+                                
+                                $.each( data.devicestatus, function( key, value )
+                                {
+                                    cameras.push(value);
+                                });
+                                
+                                if(i >= numNetworks)
+                                {
+                                    self.activityView.onCameras(cameras);
+                                }
+                            },
+                            function(data)
+                            {
+                                self.activityView.showError(data);
+                            })); 
+                        });
+                    },
+                    function(data)
+                    {
+                        self.activityView.showError(data);
+                    }));
+            },
+            enumerable: false
+        },
         getVideos : {
             value: function(i = 0)
             {
--- js/activity/ActivityView.js
+++ js/activity/ActivityView.js
@@ -1,6 +1,7 @@
 (function(views)
 {
     var self;
+    var timeline;
 
     function ActivityView(presenter)
     {
@@ -25,8 +26,7 @@
                 
                 activity.click(function(evt)
                 {
-                    self.initCalendar();
-                    self.presenter.getVideos();
+                    self.presenter.getCameras();
                     evt.preventDefault();
                 });
                 
@@ -34,22 +34,76 @@
             },
             enumerable: false
         },
-        initCalendar : {
-            value: function()
+        onCameras : {
+            value: function(data)
+            {
+                this.initTimeline(data);
+            },
+            enumerable: false
+        },
+        initTimeline : {
+            value: function(cameras)
             {
                 $(".header > div > span").html("Activity");
-                $(".content").html("<div class='calendar'></div>");
+                $(".content").html("<div class='progress mdl-progress mdl-js-progress mdl-progress__indeterminate'></div><div class='timeline'><ul class='timeline-events'></ul></div>");
                 
-               $('.content .calendar').fullCalendar({
-                  editable: false,
-                  eventLimit: true, // allow "more" link when too many events
-                  events: [],
-                  header: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'month,basicWeek,basicDay'
-                  }
-                });
+                componentHandler.upgradeAllRegistered();
+                
+                var list = [];
+                
+                for(var i = 0; i < cameras.length; ++i)
+                {
+                    list.push("<a data-idx='" + (i + 1) + "' id='timeline_camera" + cameras[i].camera_id + "' href='#'>" + cameras[i].name + "</a>");
+                }
+                
+                var options = {
+                    type          : 'point',
+                    rangeAlign	  : 'left',
+                    width           : "60vw",
+                    height          : "auto",
+                    minGridSize     : 100,
+                    marginHeight    : 5,
+                    scale         : 'day',
+                    rowHeight     : 100,
+                    loader        : false,
+                    headline      : {
+                        display   : true,
+                        title     : '',
+                        range     : true,
+                        locale    : 'en-US',
+                        format    : { timeZone: 'Europe/Brussels', hour12: false, year: 'numeric', month: 'long', day: 'numeric' }
+                    },
+                    footer        : {
+                        display   : true,
+                        content   : '',
+                        range     : true,
+                        locale    : 'en-US',
+                        format    : { hour12: false, year: 'numeric', month: 'short', day: '2-digit' }
+                    },
+                    sidebar       : {
+                        sticky : true,
+                        list   : list
+                    },
+                    ruler         : {
+                        top    : {
+                            lines      : [ 'year', 'month', 'day', 'hour' ],
+                            height     : 23,
+                            fontSize   : 14,
+                            color      : '#777',
+                            background : '#FFF',
+                            locale     : 'en-US',
+                            format     : { timeZone: 'Europe/Brussels', hour12: false, decade: 'ordinal', lustrum: 'ordinal', year: 'zerofill', month: 'long', weekday: 'long', hour: 'numeric', minute: 'numeric' },
+                        }
+                    },
+                    eventMeta       : {
+                        display     : false,
+                        scale       : 'hour',
+                    },
+                    zoom          : true
+                };
+                
+               this.timeline = $('.content .timeline').Timeline(options);
+               this.presenter.getVideos();
             },
             enumerable: false
         },
@@ -61,15 +115,17 @@
                 $.each( data, function( key, value )
                 {
                     events.push({
-                      title: value.camera_name,
-                      start: moment(value.created_at).toString(),
-                      end: moment(value.created_at).add(value.length, 'seconds').toString(),
-                      allDay: false
+                      label: value.camera_name,
+                      content: value.camera_name,
+                      start: moment(value.created_at).format("YYYY-MM-DD hh:mm:ss"),
+                      end: moment(value.created_at).add(value.length, 'seconds').format("YYYY-MM-DD hh:mm:ss"),
+                      row: $("#timeline_camera" + value.camera_id).data("idx")
                     });
                 });
                 
-                $('.content .calendar').fullCalendar('renderEvents', events, true);
-                console.log(events);
+                this.timeline.Timeline('addEvent', events);
+                
+                $(".content .progress").hide();
             },
             enumerable: false
         },
