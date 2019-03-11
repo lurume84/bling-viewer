6d69df868b626584d7693bf325754b8dd9eac4f0
lurume84 <lurume84@gmail.com>
2019-03-11 13:07:57 +0100

    thumbnail request is now working

--- js/camera/CameraView.js
+++ js/camera/CameraView.js
@@ -1,5 +1,7 @@
 (function(views)
 {
+    var self;
+    
     function CameraView(presenter)
     {
         self = this;
@@ -18,8 +20,36 @@
         load : {
             value: function(data)
             {
-                self.card = $("<div/>", {class: "demo-card-square mdl-card mdl-shadow--2dp"});
+                var card = $("<div/>", {id: "card" + data.camera_id, class: "demo-card-square mdl-card mdl-shadow--2dp"});
                     
+                var serial = " (" + data.serial + ")";
+                
+                var loading = "<div class='mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active'></div>";
+                
+                var title = $("<div/>", {class: "mdl-card__title mdl-card--expand", html: loading + "<h2 class='mdl-card__title-text'>" + data.name + serial + "</h2>"});
+                var text = $("<div/>", {class: "mdl-card__supporting-text", html: "<div class='description'></div><div class='icons'></div>"});
+                var actions = $("<div/>", {class: "mdl-card__actions mdl-card--border", 
+                                            html: "<a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect'>" +
+                                                    "<i class='action fas fa-camera' title='Get thumbnail'></i>" + 
+                                                    "</a>",
+                                            click: function(){self.requestThumbnail(data);}
+                                            });
+                
+                title.appendTo(card);
+                text.appendTo(card);
+                actions.appendTo(card);
+                
+                self.updateContent(card, data);
+                
+                card.appendTo($(".content"));
+                
+                componentHandler.upgradeDom();
+            },
+            enumerable: false
+        },
+        updateContent : {
+            value: function(card, data)
+            {
                 var icons = "";
 
                 if(data.ac_power)
@@ -42,26 +72,11 @@
 
                 description += moment(data.updated_at).fromNow();
                
-                var content = "<div class='description'>" + description + "</div><div class='icons'>" + icons + "</div>";    
+                card.find(".description").html(description);
+                card.find(".icons").html(icons);
+                card.find(".mdl-button").attr("disabled", false);
                 
-                var serial = " (" + data.serial + ")";
-                
-                var title = $("<div/>", {class: "mdl-card__title mdl-card--expand", html: "<h2 class='mdl-card__title-text'>" + data.name + serial + "</h2>"});
-                var text = $("<div/>", {class: "mdl-card__supporting-text", html: content});
-                var actions = $("<div/>", {class: "mdl-card__actions mdl-card--border", 
-                                            html: "<a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect'>" +
-                                                    "<i class='action fas fa-camera' title='Get thumbnail'></i>" + 
-                                                    "</a>",
-                                            click: function(){self.requestThumbnail(data);}
-                                            });
-                
-                title.appendTo(self.card);
-                text.appendTo(self.card);
-                actions.appendTo(self.card);
-                
-                self.getThumbnail(self.card, data);
-                
-                self.card.appendTo($(".content"));
+                self.getThumbnail(card, data);
             },
             enumerable: false
         },
@@ -86,10 +101,11 @@
         getThumbnail : {
             value: function(card, data)
             {
-                self.presenter.getThumbnail(data.thumbnail, function(data2)
+                self.presenter.getThumbnail(data.thumbnail, function(thumbnail)
                 {
                     var image = card.find(".mdl-card__title");
-                    image.css("background-image", "url('data:image/png;base64," + data2 + "')");
+                    image.css("background-image", "url('data:image/png;base64," + thumbnail + "')");
+                    image.find(".mdl-spinner").hide();
                 });
             },
             enumerable: false
@@ -97,14 +113,17 @@
         requestThumbnail : {
             value: function(data)
             {
-                self.card
+                var card = $("#card" + data.camera_id);
                 
-                var image = self.card.find(".mdl-card__title");
+                var image = card.find(".mdl-card__title");
                 image.css("background-image", "none");
                 
-                //image.html("<div class='mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active'></div>");
-                //componentHandler.upgradeDom();
-                console.log(data)
+                image.find(".mdl-spinner").show();
+                
+                card.find(".description").html("");
+                card.find(".icons").html("");
+                card.find(".mdl-button").attr("disabled", true);
+                
                 self.presenter.requestThumbnail(data.network_id, data.camera_id);
             },
             enumerable: false
@@ -126,7 +145,7 @@
                 {
                     self.presenter.getCamera(data.commands[0].network_id, data.commands[0].camera_id, "", function(data)
                     {
-                        self.getThumbnail(self.card, data);
+                        self.updateContent($("#card" + data.camera_id), data);
                     });
                 }
                 else
